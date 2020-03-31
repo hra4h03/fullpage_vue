82238ba82563f69c7f42caecee15501270e0da29
Hrach <hr.ovakimyan.03@gmail.com>
2020-03-31 18:25:40 +0400

    remove video js

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <template>
 	<div>
-		<!-- <div class="menu">
-			<ul>
-				<li v-for="slideName in slideNames" :key="slideName">
-					<a href="#">{{ slideName }}</a>
-				</li>
-			</ul>
-		</div>-->
 		<Fullpage />
 	</div>
 </template>
 
 <script>
-// import videojs from "video.js";
 import Fullpage from "./components/Fullpage";
 
 export default {
 	name: "App",
-	data() {
-		return {
-			slideNames: ["Memory", "I dont know", "Love", ". . . "]
-		};
-	},
 	components: {
 		Fullpage
 	}
 };
 </script>
-
-<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -3,16 +3,13 @@ import App from "./App.vue";
 // import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
 // import "./fullpage.scrollHorizontally.min"; // Optional. When using fullpage extensions
 import VueFullPage from "vue-fullpage.js";
-import "video.js/dist/video-js.css";
 
 Vue.use(VueFullPage);
 
 import "./registerServiceWorker";
-import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
   render: h => h(App)
 }).$mount("#app");
